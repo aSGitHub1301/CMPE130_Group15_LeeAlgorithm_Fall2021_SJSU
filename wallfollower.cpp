670526370d6c9862e8eb70eab10427aa17f2cae7
aSGitHub1301 <95245913+aSGitHub1301@users.noreply.github.com>
2021-12-05 22:00:22 -0800

    Update wallfollower.cpp

--- wallfollower.cpp
+++ wallfollower.cpp
@@ -11,7 +11,7 @@ using namespace std::chrono;
 #define ROW 10
 #define COL 10
  
-//To store matrix cell coordinates
+//store matrix cell coordinates
 struct Point
 {
     int x;
@@ -21,61 +21,45 @@ struct Point
 // A Data Structure for queue used in BFS
 struct queueNode
 {
-    Point pt;  // The coordinates of a cell
-    int dist;  // cell's distance of from the source
+    Point pt;  
+    int dist;  
 };
  
-// check whether given cell (row, col) is a valid
-// cell or not.
+// checks if cell is in matrix
 bool isValid(int row, int col)
 {
-    // return true if row number and column number
-    // is in range
     return (row >= 0) && (row < ROW) &&
            (col >= 0) && (col < COL);
 }
  
-// These arrays are used to get row and column
-// numbers of 4 neighbours of a given cell
+//directions to navigate rows and columns
 int rowNum[] = {-1, 0, 0, 1};
 int colNum[] = {0, -1, 1, 0};
  
-// function to find the shortest path between
-// a given source cell to a destination cell.
+//find the shortest path between source and destination
 int BFS(int mat[][COL], Point src, Point dest)
 {
-    // check source and destination cell
-    // of the matrix have value 1
     if (!mat[src.x][src.y] || !mat[dest.x][dest.y])
         return -1;
  
     bool visited[ROW][COL];
     memset(visited, false, sizeof visited);
      
-    // Mark the source cell as visited
     visited[src.x][src.y] = true;
  
-    // Create a queue for BFS
     queue<queueNode> q;
      
-    // Distance of source cell is 0
     queueNode s = {src, 0};
-    q.push(s);  // Enqueue source cell
+    q.push(s);  
  
-    // Do a BFS starting from source cell
     while (!q.empty())
     {
         queueNode curr = q.front();
         Point pt = curr.pt;
  
-        // If we have reached the destination cell,
-        // we are done
         if (pt.x == dest.x && pt.y == dest.y)
             return curr.dist;
  
-        // Otherwise dequeue the front
-        // cell in the queue
-        // and enqueue its adjacent cells
         q.pop();
  
         for (int i = 0; i < 4; i++)
@@ -83,12 +67,9 @@ int BFS(int mat[][COL], Point src, Point dest)
             int row = pt.x + rowNum[i];
             int col = pt.y + colNum[i];
              
-            // if adjacent cell is valid, has path and
-            // not visited yet, enqueue it.
             if (isValid(row, col) && mat[row][col] &&
                !visited[row][col])
             {
-                // mark cell as visited and enqueue it
                 visited[row][col] = true;
                 queueNode Adjcell = { {row, col},
                                       curr.dist + 1 };
@@ -96,12 +77,8 @@ int BFS(int mat[][COL], Point src, Point dest)
             }
         }
     }
- 
-    // Return -1 if destination cannot be reached
     return -1;
 }
- 
-// Driver program to test above function
 int main()
 {
     int mat[ROW][COL] =
@@ -135,4 +112,4 @@ int main()
         cout << "Time taken: " << duration.count() << " microseconds." << endl;
     }
     return 0;
-}
+};
